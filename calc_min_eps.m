72c5ef84f33c4650fbd06ad4ae2f1509ffdedb25
jgreen83 <42304272+jgreen83@users.noreply.github.com>
2026-05-16 23:37:47 -0700

    function to automatically find the stability radius woohooo maybe need to adjust auto ubd but I think it should work

---
+++ calc_min_eps.m
@@ -0,0 +1,10 @@
+function eps = calc_min_eps(A,varargin)
+    addpath '/Users/Greencat/Spring2026-Connectome-Pseudospectra/eigtool-master/num_comp/pseudo_abscissa';
+    if nargin < 2
+        lbd = 0;
+        ubd = 1;
+    end
+    abs_func = @(x) pspa_2way(A,x);
+    eps = fzero(abs_func,[lbd ubd]);
+
+end
