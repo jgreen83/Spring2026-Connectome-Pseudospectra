5520034c18e951babaebacbdca0234e47b5221e6
jgreen83 <42304272+jgreen83@users.noreply.github.com>
2026-05-21 15:33:52 -0700

    sven integral metric in 2D

---
+++ matExpNorm/integral_comparison2D.m
@@ -0,0 +1,65 @@
+A = [-1 5; 0 -2];
+% dim = 2;
+% A = randn(dim); 
+% [Q, ~] = qr(A);       % Q is a random orthogonal/unitary matrix
+% R = triu(randn(dim));
+% A = Q*R;
+% mReal = max(real(eig(A)));
+% if (mReal > 0)
+%     % A = A*(1-10^(-2))/mReal;
+%     A = A - (mReal + 10^-2)*eye(dim);
+% end
+
+tMax = 50;
+ts = 0 : .01: tMax;
+th_ts = zeros(size(ts));
+eAs = zeros(size(ts));
+for i = 1:length(ts)
+    t = ts(i);
+    eAs(i) = norm(expm(t.*A));
+    [u,s,v] = svd(expm(t.*A));
+    vMax = v(:,1);
+    th_ts(i) = atan(vMax(2)/vMax(1)); %EXPLICITLY FOR 2D
+end
+
+c = 0;
+%from goldman supplemental materials
+%EPSILON = smallest epsilon for which sigma_eps touches x = 0
+
+eps = calc_min_eps(A); 
+disp("eps: " + eps)
+M = max(eAs);
+t_star = (1-c)/(eps*M);
+
+tsReal = 0:0.01:t_star;
+
+eAsReal = zeros(size(tsReal));
+for i = 1:length(tsReal)
+    t = tsReal(i);
+    eAsReal(i) = norm(expm(t.*A));
+end
+eAint = trapz(tsReal,eAsReal);
+
+N_ths = 100;
+thetas = 0:pi/N_ths:pi - pi/N_ths;
+eAvInts = zeros(size(thetas));
+for i = 1:length(thetas)
+    theta = thetas(i);
+    v = [cos(theta); sin(theta)];
+    eAvs = zeros(size(tsReal));
+    for j = 1:length(tsReal)
+        t = tsReal(j);
+        eAvs(j) = norm(expm(t.*A)*v);
+    end
+    eAvInts(i) = trapz(tsReal,eAvs)/eAint;
+end
+
+clf
+figure(2);
+plot(thetas,eAvInts);
+
+th_ts = mod(th_ts,pi);
+figure(3);
+plot(ts(1:round(length(ts)/2)),th_ts(1:round(length(ts)/2)));
+
+
--- matExpNorm/matexp_test.m
+++ matExpNorm/matexp_test.m
@@ -1,21 +1,38 @@
 %start with a sample matrix that I know is nonnormal
 A = [-1 5; 0 -2];
-[V,D] = eig(transpose(A)*A);
+% dim = 10;
+% A = randn(dim); 
+% [Q, ~] = qr(A);       % Q is a random orthogonal/unitary matrix
+% R = triu(randn(dim));
+% A = Q*R;
+mReal = max(real(eig(A)));
+if (mReal > 0)
+    % A = A*(1-10^(-2))/mReal;
+    A = A - (mReal + 10^-2)*eye(dim);
+end
+% [V,D] = eig(transpose(A)*A);
+[V,D] = eig(A);
 [U,T] = schur(A);
 
-ts = 0 : .01: 4;
+tMax = 50;
+ts = 0 : .01: tMax;
 eAs = zeros(size(ts));
+tic
 for i = 1:length(ts)
     t = ts(i);
     eAs(i) = norm(expm(t.*A));
 end
+toc
 
 %setting thresh c = 1 just to test
 c = 0.5;
 
 %from goldman supplemental materials
 %EPSILON = smalles epsilon for which sigma_eps touches x = 0
-eps = 10^(-.44); %NEED TO DEFINE NONMANUALLY LOL %use eigtool
+tic
+eps = calc_min_eps(A); 
+toc
+disp("eps: " + eps)
 M = max(eAs);
 t_star = (1-c)/(eps*M);
 
@@ -33,28 +50,31 @@ for i = 1:length(ts)
     t = ts(i);
     eA = expm(t.*A);
     [u,s,v] = svd(eA);
-    [u1,s1,v1] = svd(expm(t.*T));
-    y1(i) = norm(eA * v(:,1)); %this should be equal to the matrix norm
+    % [u1,s1,v1] = svd(expm(t.*T));
+    y5(i) = norm(eA * v(:,1)); %this should be equal to the matrix norm
     %plot lower bd
     y2(i) = 1 - M*eps*t;
+    y1(i) = norm(eA * v(:,1) - eA*vbig);
 
-    y3(i) = norm(expm(t.*A));
+    % y3(i) = norm(expm(t.*A));
     y4(i) = norm(eA * vbig);
+    y5(i) = norm(expm(t.*A)*(V(:,1)));
 
     % y5(i) = norm(eA*U(:,2));
 
 end
 
-
-figure;
-plot(ts,y3,'DisplayName','matrix norm')
+clf
+figure(2);
+plot(ts,eAs,'DisplayName','matrix norm')
 hold on
 plot(ts,c*ones(size(ts)),'DisplayName','y = c')
 plot(t_star*ones(size(0:.1:1.5)),0:.1:1.5,'DisplayName','t = t^*')
 %evs
-% plot(ts,y1)
+% plot(ts,y1,'DisplayName','norm of difference thing')
 plot(ts,y2,'DisplayName','lower bound')
+plot(ts,y2 - y1,'DisplayName','c - M')
 plot(ts,y4,'DisplayName','first rsv of t_star expm')
 %schur modes
-% plot(ts,y5)
+plot(ts,y5)
 legend
